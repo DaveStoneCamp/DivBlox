65d8e8a482c2f6c3199ceb6a790391f91cca7363
Dawid <dave2.stonecamp2@gmail.com>
2023-04-21 10:37:44 +0200

    Foundation Task 3
    - Implemented a variable to hold word
    - Used while loop to check for condition to stop
    
    https://trello.com/c/yyQqx0RQ/4-foundation-task-3

--- FoundationTask3/index.php
+++ FoundationTask3/index.php
@@ -4,22 +4,52 @@
 // Write a function that checks if a given word is a palindrome. Character case should be ignored.
 // For example, isPalindrome("Never Odd Or Even") should return true as character case should be ignored, resulting in "Never Odd Or Even", which is a palindrome since it reads the same backward and forward.
 
+//ATTEMPT 1
+// class Palindrome
+// {
+//     public static function isPalindrome($word)
+//     {
+//         //TODO: Implement this
+//         $word = strtolower(str_replace(" ", "", $word));
+//         return $word == strrev($word);
+//         // return false;
+//     }
+// }
 
+// if (Palindrome::isPalindrome('Dont nod')){
+//     echo 'Palindrome';
+// } else{
+//     echo 'Not palindrome';
+// }
 
-class Palindrome
-{
-    public static function isPalindrome($word)
-    {
-        //TODO: Implement this
-        $word = strtolower(str_replace(" ", "", $word));
-        return $word == strrev($word);
-        // return false;
+//After TODO is done delete.
+//Coding standards
+//Pal string in variable
+
+//ATTEMPT 2
+class Palindrome {
+    public static function isPalindrome($strInputWord) {
+        // Remove special characters and spaces from the word and Convert the word to lowercase
+        $strWord = strtolower(preg_replace('/[^A-Za-z0-9]/', '', $strInputWord));
+
+        // Reverse the word
+        $strWordReverse = strrev($strWord);
+        
+        // Check if the word is equal to its reverse
+        if ($strWord == $strWordReverse) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
-if (Palindrome::isPalindrome('Never Odd Or Even')){
+$strInputWord = "   $ d;';[ o ./;'[d";
+
+if (Palindrome::isPalindrome($strInputWord)) {
     echo 'Palindrome';
-} else{
+} else {
     echo 'Not palindrome';
 }
+
 ?>
