fc5ac737dca95b0e6e0aa1b6dd28a29e62830aab
Dawid <dave2.stonecamp2@gmail.com>
2023-04-21 10:41:59 +0200

    Foundation Task 6
    - Implemented deletePerson method
    - Implemented loadAllPeople method
    - Implemented deleteAllPeople method
    - Implemented loadPerson method
    - Implemented createPerson method
    
    https://trello.com/c/2MixOEXd/7-foundation-task-6

--- FoundationTask6/index.php
+++ FoundationTask6/index.php
@@ -14,28 +14,28 @@
 // II. Implement the loadAllPeople function, this function should return all the people's information, then loop over the returned information and print each Person's information to the screen
 // III. Add a log that logs when your script starts and ends and shows how long it took to execute
 
-$time_start = microtime(true);
+$FltTimeStart = microtime(true);
 
-$host = "localhost";
-$user = "admin";
-$password = "";
-$database = "divblox_training_db";
+$StrHost = "localhost";
+$StrUser = "admin";
+$MixedPassword = "";
+$StrDatabase = "divblox_training_db";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$MixedConn = mysqli_connect($StrHost, $StrUser, $MixedPassword, $StrDatabase);
 
-class Person
-{
+class Person {
   private $id;
   private $firstName;
   private $surname;
   private $dateOfBirth;
   private $emailAddress;
   private $age;
-
-  public function createPerson($firstName, $surname, $dateOfBirth, $emailAddress, $age)
-  { 
-    global $conn;
-    $sqlCreateQuery = "INSERT INTO Person (FirstName, Surname, DateOfBirth, EmailAddress, Age) 
+  function __construct()  {
+  }
+  public function createPerson($firstName, $surname, $dateOfBirth, $emailAddress, $age)  {
+    //create a new person
+    global $MixedConn;
+    $SqlCreateQuery = "INSERT INTO Person (FirstName, Surname, DateOfBirth, EmailAddress, Age) 
             VALUES ('$firstName', '$surname', '$dateOfBirth', '$emailAddress', $age)";
 
     $this->firstName = $firstName;
@@ -43,60 +43,66 @@ class Person
     $this->emailAddress = $emailAddress;
     $this->age = $age;
     $this->dateOfBirth = $dateOfBirth;
-    return mysqli_query($conn, $sqlCreateQuery);
+    return mysqli_query($MixedConn, $SqlCreateQuery);
   }
 
-  public function loadPerson($id)
-  {
-    //TODO: load person from database using id
+  public function loadPerson($id)  {
+    //load person from database using id
+    global $MixedConn;
+    $SqlLoadQuery = "SELECT * FROM Person WHERE id = $id";
+    $MixedData = mysqli_query($MixedConn, $SqlLoadQuery);
+    $entry = mysqli_fetch_assoc($MixedData);
+    echo $entry['id'] . $entry['FirstName'] . $entry['Surname'] . $entry['DateOfBirth'] . $entry['EmailAddress'] . $entry['Age'] . "</br>";
   }
 
-  public function savePerson()
-  {
+  public function savePerson()  {
     //TODO: save changes to person in database
   }
 
-  public function deletePerson()
-  {
-    //TODO: delete person from database
+  public function deletePerson($id)  {
+    //delete person from database
+    global $MixedConn;
+    $SqlLoadQuery = "DELETE FROM Person WHERE id = $id";
+    return mysqli_query($MixedConn, $SqlLoadQuery);
   }
 
-  public function loadAllPeople()
-  {
-    //TODO: load all people from database
-    global $conn;
-    $sqlLoadQuery = "SELECT * FROM Person";
-    $data = mysqli_query($conn, $sqlLoadQuery);
-    $checkRows = mysqli_num_rows($data);
+  public function loadAllPeople()  {
+    //load all people from database
+    global $MixedConn;
+    $SqlLoadQuery = "SELECT * FROM Person";
+    $MixedData = mysqli_query($MixedConn, $SqlLoadQuery);
+    $checkRows = mysqli_num_rows($MixedData);
     if ($checkRows > 0) {
-      while ($entry = mysqli_fetch_assoc($data)) {
-        echo $entry['id'].$entry['FirstName'].$entry['Surname'].$entry['DateOfBirth'].$entry['EmailAddress'].$entry['Age']."</br>";
+      while ($entry = mysqli_fetch_assoc($MixedData)) {
+        echo $entry['id'] . $entry['FirstName'] . $entry['Surname'] . $entry['DateOfBirth'] . $entry['EmailAddress'] . $entry['Age'] . "</br>";
       }
     }
   }
 
-  public function deleteAllPeople()
-  {
-    //TODO: delete all people from database
+  public function deleteAllPeople()  {
+    //delete all people from database
+    global $MixedConn;
+    $SqlDeleteAllQuery = "DELETE FROM Person;";
+    return mysqli_query($MixedConn, $SqlDeleteAllQuery);
   }
 }
 
 //create a new Person
 $person = new Person();
 
-$firstNames = array("John", "Jane", "David", "Samantha", "Michael", "Emily", "Christopher", "Olivia", "Sophia", "William");
-$surnames = array("Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor");
-$dobs = array("1990-01-15", "1985-03-22", "1995-07-10", "1982-09-05", "1998-12-30", "1989-04-17", "1993-06-02", "1991-11-11", "1987-08-24", "1996-02-19");
-$ages = array(31, 36, 26, 39, 23, 32, 28, 30, 34, 25);
-$emails = array("john.smith@example.com", "jane.johnson@example.com", "david.williams@example.com", "samantha.jones@example.com", "michael.brown@example.com", "emily.davis@example.com", "chris.miller@example.com", "olivia.wilson@example.com", "sophia.moore@example.com", "william.taylor@example.com");
+// $ArrFirstNames = array("John", "Jane", "David", "Samantha", "Michael", "Emily", "Christopher", "Olivia", "Sophia", "William");
+// $ArrSurnames = array("Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor");
+// $ArrDateOfBrithDates = array("1990-01-15", "1985-03-22", "1995-07-10", "1982-09-05", "1998-12-30", "1989-04-17", "1993-06-02", "1991-11-11", "1987-08-24", "1996-02-19");
+// $ArrAges = array(31, 36, 26, 39, 23, 32, 28, 30, 34, 25);
+// $ArrEmails = array("john.smith@example.com", "jane.johnson@example.com", "david.williams@example.com", "samantha.jones@example.com", "michael.brown@example.com", "emily.davis@example.com", "chris.miller@example.com", "olivia.wilson@example.com", "sophia.moore@example.com", "william.taylor@example.com");
 
-for ($i = 0; $i < 10; $i++) {
-  $person->createPerson($firstNames[$i], $surnames[$i], $dobs[$i], $emails[$i], $ages[$i]);
-}
+// for ($IntIndex = 0; $IntIndex < 10; $IntIndex++) {
+//   $person->createPerson($ArrFirstNames[$IntIndex], $ArrSurnames[$IntIndex], $ArrDateOfBrithDates[$IntIndex], $ArrEmails[$IntIndex], $ArrAges[$IntIndex]);
+// }
 
-$person->loadAllPeople();
+$person->loadPerson(56);
 
-$time_end = microtime(true);
+$FltTimeEnd = microtime(true);
 
-$execution_time = ($time_end - $time_start) * 100;
-echo '</br></br><b>Total Execution Time:</b> ' . $execution_time . '<b>ms</b>';
+$FltExecutionTime = ($FltTimeEnd - $FltTimeStart) * 100;
+echo '</br></br><b>Total Execution Time:</b> ' . $FltExecutionTime . '<b>ms</b>';
